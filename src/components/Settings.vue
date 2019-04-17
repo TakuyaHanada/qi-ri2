c438786307dc828e8520c0d767f54152246b4b02
TakuyaHanada <takuyahanada@TakuyaHanadanoMacBook-Pro.local>
2019-04-17 22:17:28 +0900

    first commit

---
+++ src/components/Settings.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12>
+        <v-card>
+          <v-list two-line subheader>
+            <v-subheader>アプリ情報</v-subheader>
+
+            <v-list-tile avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>最終更新日</v-list-tile-title>
+                <v-list-tile-sub-title>2019/04/17</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>バージョン</v-list-tile-title>
+                <v-list-tile-sub-title>2.0</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile avatar>
+              <v-list-tile-content>
+                <v-list-tile-title>開発者</v-list-tile-title>
+                <v-list-tile-sub-title>TakuyaHanada</v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
